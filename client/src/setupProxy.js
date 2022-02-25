6a0f5a3e5b2ed2d05cb30ddadcba92bce830e31d
Kshitij <pawarkshitij888@gmail.com>
2022-02-25 17:36:27 +0530

    update proxy to be different on dev and production

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
+  let proxyURL = 'http://localhost:8080/'
+
+  if (process.env.NODE_ENV === 'production') {
+    proxyURL = 'https://md-note-taker.herokuapp.com/'
+  }
+
   app.use(
     createProxyMiddleware('/api', {
-      target: 'https://md-note-taker.herokuapp.com/',
+      target: proxyURL,
       changeOrigin: true,
     })
   );
