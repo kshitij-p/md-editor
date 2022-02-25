818ad1c9e6235050e6cc15cd4f2f4bb5d905e916
Kshitij <pawarkshitij888@gmail.com>
2022-02-25 16:06:08 +0530

    update server config for deployment

--- server.js
+++ server.js
@@ -100,6 +100,14 @@ passport.deserializeUser(async (id, done) => {
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+if(process.env.NODE_ENV === production){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 app.use(userRouter);
 app.use(filesRouter);
 
