c05d003b9a9cbf9fdcb9b921862b845c0b1f1132
Kshitij <pawarkshitij888@gmail.com>
2022-02-25 16:20:05 +0530

    fix routing order bug in server.js

--- server.js
+++ server.js
@@ -102,10 +102,7 @@ app.use(express.urlencoded({ extended: true }))
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+    
 }
 
 app.use(userRouter);
@@ -178,6 +175,14 @@ app.post('/api/parsefile', upload.single('file'), async (req, res) => {
     return res.status(200).json({ message: "Successfully parsed the requested file", parsedFile: parsedFile });
 
 })
+
+if(process.env.NODE_ENV === "production"){
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+    
+}
 
 
 app.listen(port, (e) => {
